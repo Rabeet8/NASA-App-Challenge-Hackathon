3d5c434c9b59cae6fa261c400d0d0823d566f162
Rabeet8 <syedrabeet2002@gmail.com>
2022-10-02 20:56:18 +0500

    initial commit

---
+++ Quizz game/js/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Why is james web telescope is so important?",
+    answer: "It can see deeper in the universe than it's predecessor",
+    options: [
+      "It can capture amazing pictures",
+      "It is the most advance telescope",
+      "It can see deeper in the universe than it's predecessor",
+      "It can not be replicated"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How many important components does JWST?",
+    answer: "10 components",
+    options: [
+      "10 components",
+      "5 components",
+      "7 components",
+      "4 components"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does Trim flap use for?",
+    answer: "Help stabilize the telescope",
+    options: [
+      "Help the telescope move faster",
+      "Help the telescope capture image",
+      "Help the telescope safe from dangers",
+      "Help stabilize the telescope"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How far back will JWST see?",
+    answer: "100 million year",
+    options: [
+      "50 million year",
+      "30 million year",
+      "100 million year",
+      "70 million year"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does Solar Power Array used for?",
+    answer: "Converts sunlight into electricity to power observity",
+    options: [
+      "Converts sunlight into electricity to power observity",
+      "Converts sunlight into mechanical energy to power observity",
+      "Observes sunlight to power observity",
+      "To protect telescope panels from sunlight"
+    ]
+  },
+
+];
